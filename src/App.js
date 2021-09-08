a6a8863631b1a55bcc6463e44b017a04264b7e30
Templars <antoine.ratat@gmail.com>
2021-09-08 17:11:26 +0800

    update readme.md

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import './App.css'
+import ToggleComponent from './hooks/toggleComponent/ToggleComponent'
+import TimeoutComponent from './hooks/timeoutComponent/TimeoutComponent'
+import DebounceComponent from './hooks/debounceComponent/DebounceComponent'
+import UpdateEffectComponent from './hooks/updateEffectComponent/UpdateEffectComponent'
+import ArrayComponent from './hooks/arrayComponent/ArrayComponent'
 
 function App() {
-	return <div className='App'></div>
+	return (
+		<div className='App'>
+			<ToggleComponent />
+			{/* <TimeoutComponent /> */}
+			{/* <DebounceComponent /> */}
+			{/* <UpdateEffectComponent /> */}
+			{/* <ArrayComponent /> */}
+		</div>
+	)
 }
 
 export default App
